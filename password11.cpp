e57bbdf997ac71e342f2a59cb4e3741af3795220
Madhur sengar <42689441+sengarmadhur@users.noreply.github.com>
2019-10-09 11:50:49 +0530

    Add files via upload

---
+++ password11.cpp
@@ -0,0 +1,24 @@
+//MS
+#include<iostream>
+#include<string.h>
+#include<algorithm>
+#define int long long int
+const int mod =1000000007;
+using namespace std;
+int gcd(int a,int b){
+    if(b==0)return a;
+    return gcd(b,a%b);
+}
+int32_t main(){
+    int t;cin>>t;
+    while(t--){
+        int ans=0,n;cin>>n;
+
+        for(int x=1;x<n;x++){
+            int g = gcd(x,n);
+            ans = (ans + (n*x/g)%mod)%mod;
+        }
+        printf("%d\n",ans);
+    }
+}
+
