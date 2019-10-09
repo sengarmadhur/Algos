d0bba97cc8f4d167578c971f5e380387023b74f7
Madhur sengar <42689441+sengarmadhur@users.noreply.github.com>
2019-10-09 12:00:59 +0530

    Update Hidden_Palindrome.cpp

--- Hidden_Palindrome.cpp
+++ Hidden_Palindrome.cpp
@@ -4,12 +4,13 @@
 #include<algorithm>
 #define int long long int
 using namespace std;
+//checking from i'th index to j'th index only 
 bool ispalin(string s,int i,int j){
 
-  if(i>j)return false;
-  if(s[i]==s[j]&&i==j-1)return true;
-  if(s[i]==s[j])return ispalin(s,i+1,j-1)||i==j-2;
-  else return ispalin(s,i+1,j)||ispalin(s,i,j-1);
+  if(i>j)return false; //Base case no palindrome found
+  if(s[i]==s[j]&&i==j-1)return true;//case when "xx" type palindrome found
+  if(s[i]==s[j])return ispalin(s,i+1,j-1)||i==j-2;//check from i+1 to j-1 or "xyx" type palindrome found
+  else return ispalin(s,i+1,j)||ispalin(s,i,j-1);//no matching
 }
 int32_t main(){
     int t;cin>>t;
