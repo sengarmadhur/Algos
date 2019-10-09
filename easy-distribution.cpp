e011fe346a5e4c61d586975ee2b36f89a5c0898f
Madhur sengar <42689441+sengarmadhur@users.noreply.github.com>
2019-10-09 11:42:08 +0530

    Add files via upload

---
+++ easy-distribution.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long int
+const int MOD =1000000007;
+ull fact[100001];
+void factorial()
+{   fact[0]=1;
+    for(int i=1;i<100001;i++)
+    {
+        fact[i] = (fact[i-1]*i)%MOD;
+    }
+}
+// (n!%m)*((r!)^m-2)%m)*(((n-r)!)^m-2)%m)%m
+ull POWER(ull x,ull y)
+{
+    if(y==0)return 1;
+    if(y%2==0)
+    {
+        x = POWER((x*x)%MOD,y/2);
+
+    }
+    else{
+        x = x*POWER(x,y-1)%MOD;
+    }
+    return x;
+}
+ull ncr(int n,int r)
+{
+    ull ans = fact[n]%MOD;
+    ans = ((ans*(POWER(fact[r],MOD-2)%MOD)%MOD)*(POWER(fact[n-r],MOD-2)%MOD))%MOD;
+    return ans;
+}
+int main()
+{
+    factorial();
+    int n,m; cin>>n>>m;
+    ull ans=0;
+    ull k = n/m;
+    for(ull i=1;i<=k;i++){
+    ull p=1;
+    int temp =n+i;
+    for(int j=0;j<m;j++){
+        temp-=i;
+        p*=ncr(temp,i);
+        p%=MOD;
+        //cout<<p<<endl;
+    }
+    ans+=p;
+    ans%=MOD;
+    }
+    cout<<ans;
+
+
+}
+
